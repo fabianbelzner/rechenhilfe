6e105c96136ad0caf4f5181a31938e8c648a6999
fabianbelzner <belzner@gmail.com>
2025-05-16 21:13:57 +0200

    new design: ypace

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('abaco-cache').then(cache => {
+    caches.open('rechenhilfe-cache').then(cache => {
       return cache.addAll([
         './index.html',
         './manifest.json',
@@ -13,7 +14,6 @@ self.addEventListener('install', event => {
     })
   );
 });
-
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(resp => {
